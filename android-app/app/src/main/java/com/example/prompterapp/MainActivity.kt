21e555e5c2a5fbc1443a919536bd1c625447fa54
a.shunyaev <a.v.shunaev@gmail.com>
2023-02-28 23:54:31 +0300

    feat(android-app): add main screen (full mock)

--- android-app/app/src/main/java/com/example/prompterapp/MainActivity.kt
+++ android-app/app/src/main/java/com/example/prompterapp/MainActivity.kt
@@ -18,16 +18,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val title = findViewById<TextView>(R.id.title)
-        val reports = findViewById<ListView>(R.id.reportList)
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strings)
-
-        reports.adapter = adapter
-        reports.onItemClickListener = OnItemClickListener { parent, v, position, id -> // по позиции получаем выбранный элемент
-            val selectedItem: String = strings[position]
-            // установка текста элемента TextView
-            title.text = selectedItem
-        }
+//        val title = findViewById<TextView>(R.id.title)
+//        val reports = findViewById<ListView>(R.id.reportList)
+//        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strings)
+//
+//        reports.adapter = adapter
+//        reports.onItemClickListener = OnItemClickListener { parent, v, position, id -> // по позиции получаем выбранный элемент
+//            val selectedItem: String = strings[position]
+//            // установка текста элемента TextView
+//            title.text = selectedItem
+//        }
     }
 
     override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
