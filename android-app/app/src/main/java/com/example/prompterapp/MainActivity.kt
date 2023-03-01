e941942531f9007b12f30920410134178e587bc2
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2023-03-01 20:01:53 +0300

    feat: add create/delete popup

--- android-app/app/src/main/java/com/example/prompterapp/MainActivity.kt
+++ android-app/app/src/main/java/com/example/prompterapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.prompterapp
 
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.AttributeSet
@@ -8,9 +10,14 @@ import android.view.View
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,6 +42,20 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.doc1).setOnClickListener(presentationClickListener)
         findViewById<Button>(R.id.doc2).setOnClickListener(presentationClickListener)
         findViewById<Button>(R.id.doc3).setOnClickListener(presentationClickListener)
+
+        findViewById<Button>(R.id.add).setOnClickListener {
+            CreateNew().show(this.supportFragmentManager, "Adding")
+        }
+
+        findViewById<ImageView>(R.id.delete).setOnClickListener {
+            DeleteNew().show(this.supportFragmentManager, "Deleting")
+        }
+        findViewById<ImageView>(R.id.delete2).setOnClickListener {
+            DeleteNew().show(this.supportFragmentManager, "Deleting2")
+        }
+        findViewById<ImageView>(R.id.delete3).setOnClickListener {
+            DeleteNew().show(this.supportFragmentManager, "Deleting3")
+        }
     }
 
     override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
